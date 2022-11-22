1c6bd5a71c5865fbb9db6d4375e487973ef171c0
Mudassir <mudassirnawazjavaid@gmail.com>
2022-11-22 21:53:05 +0500

    initial pc version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
 const navBtn = document.querySelector('#navBtnOpen');
 const menu = document.querySelector('#mobileMenu')
+const body = document.body;
+const menuItem = document.querySelectorAll('.menu-item')
+const menubarIcon = document.querySelector('#menubar-icon');
+
 navBtn.addEventListener('click', ()=>{
     menu.classList.toggle('hidden')
     menu.classList.toggle('block')
 })
+
+menuItem.forEach((item)=> {
+    item.addEventListener('click', ()=> {
+        menu.classList.toggle('hidden')
+        menu.classList.toggle('block')
+    })
+})
+
+window.addEventListener('scroll', (event) => {
+
+    menu.classList.add('hidden')
+    if(menubarIcon.classList.contains('fa-xmark')){
+        menubarIcon.classList.toggle('fa-xmark');
+        menubarIcon.classList.toggle('fa-bars');
+    }
+
+});
+
+
+
+
+menubarIcon.addEventListener('click', ()=> {
+    menubarIcon.classList.toggle('fa-bars');
+    menubarIcon.classList.toggle('fa-xmark');
+})
+
