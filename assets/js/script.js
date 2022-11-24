5c65d6e1b88311c7f544478be1de95cfce038868
Mudassir <mudassirnawazjavaid@gmail.com>
2022-11-24 18:33:57 +0500

    new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('#mobileMenu')
 const body = document.body;
 const menuItem = document.querySelectorAll('.menu-item')
 const menubarIcon = document.querySelector('#menubar-icon');
+const nav = document.querySelector('#navbar');
 
 navBtn.addEventListener('click', ()=>{
     menu.classList.toggle('hidden')
@@ -16,18 +17,20 @@ menuItem.forEach((item)=> {
     })
 })
 
-window.addEventListener('scroll', (event) => {
+// window.addEventListener('scroll', (event) => {
 
-    menu.classList.add('hidden')
-    if(menubarIcon.classList.contains('fa-xmark')){
-        menubarIcon.classList.toggle('fa-xmark');
-        menubarIcon.classList.toggle('fa-bars');
-    }
+//     // nav.classList.remove('bg-transparent')
+//     // nav.classList.add('bg-white')
 
-});
+//     menu.classList.add('hidden')
+//     if(menubarIcon.classList.contains('fa-xmark')){
+//         menubarIcon.classList.toggle('fa-xmark');
+//         menubarIcon.classList.toggle('fa-bars');
+//     }
+
+// });
 
 menubarIcon.addEventListener('click', ()=> {
     menubarIcon.classList.toggle('fa-bars');
     menubarIcon.classList.toggle('fa-xmark');
 })
-
