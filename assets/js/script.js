c0659a7109b041ba82417598e63fcab455ef6689
Mudassir <mudassirnawazjavaid@gmail.com>
2022-11-25 18:43:12 +0500

    navbar changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const body = document.body;
 const menuItem = document.querySelectorAll('.menu-item')
 const menubarIcon = document.querySelector('#menubar-icon');
 const nav = document.querySelector('#navbar');
+const navItems = document.querySelector('#nav-item-list').children;
 
 navBtn.addEventListener('click', ()=>{
     menuBar1.classList.toggle('hidden')
@@ -32,3 +33,11 @@ menubarIcon.addEventListener('click', ()=> {
     menubarIcon.classList.toggle('fa-bars');
     menubarIcon.classList.toggle('fa-xmark');
 })
+
+
+for (let v = 0; v < navItems.length; v++ ) {
+    navItems[v].addEventListener('click', function() {
+        menubarIcon.classList.toggle('fa-xmark');
+        menubarIcon.classList.toggle('fa-bars');
+    })
+}
